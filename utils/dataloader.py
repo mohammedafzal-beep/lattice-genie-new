d14b27fbcd062d2e6b0d57515a3ffa9f8bd7e1b5
mohammedafzal-beep <mo0002al@e.ntu.edu.sg>
2025-09-30 18:52:39 +0800

    Update dataloader.py

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -10,5 +10,6 @@ def load_data():
         "crystal_images": load_json("crystal_images.json"),
         "subtypes_info": load_json("subtype_info.json"),
         "params_dict": {int(k): v for k, v in load_json("param_Dict.json").items()},
+        "dict_key_map": {int(k): v for k, v in load_json("dict_key_map.json").items()},
         "system_prompt": open("instruction.txt").read()
     }
